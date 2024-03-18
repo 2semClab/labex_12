040e98f2913beda891d4153343ddb704dcdb167c
2semClab <158600040+2semClab@users.noreply.github.com>
2024-03-18 12:58:16 +0530

    Update 4.c

--- 4.c
+++ 4.c
@@ -15,14 +15,14 @@ return 0;
 }
 int isPrime(int n){
 int flag = 0;
-	for(int i = 0; i<n; i++){
+	for(int i = 2; i<n; i++){
 		if(n % i == 0){
 			flag =1;
 			break;
 		}
 	}
 if (flag == 1){
-	return 1;
+	return 0;
 }
 }
 
