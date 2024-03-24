0effd9b3ebb7b3a8746219a3255ed87a8fc5a118
2semClab <158600040+2semClab@users.noreply.github.com>
2024-03-24 22:21:38 +0530

    Update 5.c

--- 5.c
+++ 5.c
@@ -7,18 +7,16 @@ printf("Enter the size of the array : ");
 scanf("%d",&n);
 int a[n];
 printf("Enter the elements : ");
-for(int i =0; i<n; i++){
+for(int i =0; i<n; i++)
 	scanf("%d",&a[i]);
-	}
 printf("Orginal array :");
 print(n,a);
 revArray(n,a);
 return 0;
 }
 void print(int n, int a[]){
-for(int i =0; i<n; i++){
+for(int i =0; i<n; i++)
 	printf(" %d",a[i]);
-	}
 printf("\n");
 }
 void revArray(int n, int a[]){
@@ -28,6 +26,6 @@ for(int i=0 ; i<n/2;i++){
 	a[i] = a[n-1-i];
 	a[n-i-1] = c;
 	}
-	printf("Reverse of the array :");
-	print(n,a);
+printf("Reverse of the array :");
+print(n,a);
 }
